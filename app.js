8bdb6f4b437d863749e9ef8ef85c08742ab51aa3
aim4dabush <edarasato@gmail.com>
2023-09-26 13:23:56 -0700

    added content-type header to preflight response

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 // set headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With', 'Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type', 'Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, PATCH, POST, PUT, DELETE');
 
     next();
